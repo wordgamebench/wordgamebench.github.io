6be123db0c13aaa533f96234dab3e563fa82097a
Zafir Stojanovski <zaf.stojano@gmail.com>
2024-08-18 20:21:17 +0200

    fetch data

--- fetch_data.py
+++ fetch_data.py
@@ -6,6 +6,11 @@ FILES = [
     "daily_connections.json",
 ]
 
+print("Downloading files...")
+
 s3 = boto3.client('s3')
 for filename in FILES:
     s3.download_file('wgb-s3', filename, f"./assets/{filename}")
+    print(f"Downloaded {filename}")
+
+print("Done!")
